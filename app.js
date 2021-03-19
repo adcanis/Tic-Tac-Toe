f1dbc12e872b07fce78be9f16d8b358d6d6c31df
adcanis <adikianidis@gmail.com>
2021-03-19 14:56:21 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+//Global Settings
+const newGameBtn = document.querySelector('#reset');
+const p1Display = document.querySelector('#p1Display');
+const p2Display = document.querySelector('#p2Display');
+const roundSelect = document.querySelector('#round');
+
