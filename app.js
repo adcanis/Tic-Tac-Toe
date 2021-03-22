a0dc825246ee5803aa5166b1dd4c4ec7c243f42c
adcanis <adikianidis@gmail.com>
2021-03-22 19:23:21 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -4,17 +4,6 @@ let p2Display = document.querySelector('#p2Display');
 const roundSelect = document.querySelector('#round');
 const newGameBtn = document.querySelector('#reset');
 const square = Array.from(document.querySelectorAll('#playSquare'));
-
-//Game Settings
-let turn = 'X';
-let gameBoard;
-let win;
-let p1Score = 0;
-let p2Score = 0;
-let winningScore = 3;
-let gameOver = false;
-
-
 const winningCombos = [
     [0, 1, 2],
     [3, 4, 5],
@@ -26,11 +15,20 @@ const winningCombos = [
     [2, 4, 6]
 ]
 
+let turn = 'X';
+let gameBoard;
+let win;
+let p1Score = 0;
+let p2Score = 0;
+let winningScore = 3;
+let gameOver = false;
+
 roundSelect.addEventListener('change', function() {
     winningScore = parseInt(this.value);
     newGame();
 })
 
+//Game Settings
 function newGame() {
     gameOver = false;
     p1Score = 0;
@@ -46,101 +44,114 @@ function newGame() {
     p2Display.classList.remove('has-text-success', 'has-text-danger');
     game();
 }
+document.getElementById('game').addEventListener('click', playGame);
 
-document.getElementById('game').addEventListener('click', playTurn);
-
-function getWinner() {
-    if (square[0].textContent === 'X' & square[1].textContent === 'X' & square[2].textContent === 'X') {
+function playGame() {
+    let play = square.findIndex(function(play) {
+        return play === event.target;
+    });
+    gameBoard[play] = turn;
+    turn = turn === 'X' ? 'O' : 'X';
+    if (square[0].textContent === 'X' && square[1].textContent === 'X' && square[2].textContent === 'X') {
         p1Score += 1;
         p1Display.classList.add('has-text-success');
         p1Display.innerHTML 
         p1Display.textContent = p1Score;
-        
+        alert('Player 1 wins this round!');
         gameBoard = [
             '', '', '',
             '', '', '',
             '', '', ''
-            ];          
-    } if (square[0].textContent === 'O' & square[1].textContent === 'O' & square[2].textContent === 'O') {
+            ];      
+    } if (square[0].textContent === 'O' && square[1].textContent === 'O' && square[2].textContent === 'O') {
         p2Score += 1;
         p2Display.classList.add('has-text-success');
         p2Display.innerHTML = p2Score;
+        alert('Player 2 wins this round!');
         gameBoard = [
             '', '', '',
             '', '', '',
             '', '', ''
             ];
-    }  if (square[3].textContent === 'X' & square[4].textContent === 'X' & square[5].textContent === 'X') {
+    }  if (square[3].textContent === 'X' && square[4].textContent === 'X' && square[5].textContent === 'X') {
         p1Score += 1;
         p1Display.classList.add('has-text-success');
         p1Display.innerHTML 
         p1Display.textContent = p1Score;
+        alert('Player 1 wins this round!');
         gameBoard = [
             '', '', '',
             '', '', '',
             '', '', ''
             ];          
-    } if (square[3].textContent === 'O' & square[4].textContent === 'O' & square[5].textContent === 'O') {
+    } if (square[3].textContent === 'O' && square[4].textContent === 'O' && square[5].textContent === 'O') {
         p2Score += 1;
         p2Display.classList.add('has-text-success');
         p2Display.innerHTML = p2Score;
+        alert('Player 2 wins this round!');
         gameBoard = [
             '', '', '',
             '', '', '',
             '', '', ''
             ];
-    }  if (square[6].textContent === 'X' & square[7].textContent === 'X' & square[8].textContent === 'X') {
+    }  if (square[6].textContent === 'X' && square[7].textContent === 'X' &&square[8].textContent === 'X') {
         p1Score += 1;
         p1Display.classList.add('has-text-success');
         p1Display.innerHTML 
         p1Display.textContent = p1Score;
+        alert('Player 1 wins this round!');
         gameBoard = [
             '', '', '',
             '', '', '',
             '', '', ''
             ];          
-    } if (square[6].textContent === 'O' & square[7].textContent === 'O' & square[8].textContent === 'O') {
+    } if (square[6].textContent === 'O' && square[7].textContent === 'O' && square[8].textContent === 'O') {
         p2Score += 1;
         p2Display.classList.add('has-text-success');
         p2Display.innerHTML = p2Score;
+        alert('Player 2 wins this round!');
         gameBoard = [
             '', '', '',
             '', '', '',
             '', '', ''
             ];
-    }  if (square[0].textContent === 'X' & square[3].textContent === 'X' & square[6].textContent === 'X') {
+    }  if (square[0].textContent === 'X' && square[3].textContent === 'X' && square[6].textContent === 'X') {
         p1Score += 1;
         p1Display.classList.add('has-text-success');
         p1Display.innerHTML 
         p1Display.textContent = p1Score;
+        alert('Player 1 wins this round!');
         gameBoard = [
             '', '', '',
             '', '', '',
             '', '', ''
             ];          
-    } if (square[0].textContent === 'O' & square[3].textContent === 'O' & square[6].textContent === 'O') {
+    } if (square[0].textContent === 'O' && square[3].textContent === 'O' &&square[6].textContent === 'O') {
         p2Score += 1;
         p2Display.classList.add('has-text-success');
         p2Display.innerHTML = p2Score;
+        alert('Player 2 wins this round!');
         gameBoard = [
             '', '', '',
             '', '', '',
             '', '', ''
             ];
-    }  if (square[1].textContent === 'X' & square[4].textContent === 'X' & square[7].textContent === 'X') {
+    }  if (square[1].textContent === 'X' && square[4].textContent === 'X' && square[7].textContent === 'X') {
         p1Score += 1;
         p1Display.classList.add('has-text-success');
         p1Display.innerHTML 
         p1Display.textContent = p1Score;
+        alert('Player 1 wins this round!');
         gameBoard = [
             '', '', '',
             '', '', '',
             '', '', ''
             ];          
-    } if (square[1].textContent === 'O' & square[4].textContent === 'O' & square[7].textContent === 'O') {
+    } if (square[1].textContent === 'O' && square[4].textContent === 'O' &&square[7].textContent === 'O') {
         p2Score += 1;
         p2Display.classList.add('has-text-success');
         p2Display.innerHTML = p2Score;
+        alert('Player 2 wins this round!');
         gameBoard = [
             '', '', '',
             '', '', '',
@@ -151,25 +162,28 @@ function getWinner() {
         p1Display.classList.add('has-text-success');
         p1Display.innerHTML 
         p1Display.textContent = p1Score;
+        alert('Player 1 wins this round!');
         gameBoard = [
             '', '', '',
             '', '', '',
             '', '', ''
             ];          
-    } if (square[2].textContent === 'O' & square[5].textContent === 'O' & square[8].textContent === 'O') {
+    } if (square[2].textContent === 'O' && square[5].textContent === 'O' && square[8].textContent === 'O') {
         p2Score += 1;
         p2Display.classList.add('has-text-success');
         p2Display.innerHTML = p2Score;
+        alert('Player 2 wins this round!');
         gameBoard = [
             '', '', '',
             '', '', '',
             '', '', ''
             ];
-    }  if (square[0].textContent === 'X' & square[4].textContent === 'X' & square[8].textContent === 'X') {
+    }  if (square[0].textContent === 'X' && square[4].textContent === 'X' && square[8].textContent === 'X') {
         p1Score += 1;
         p1Display.classList.add('has-text-success');
         p1Display.innerHTML 
         p1Display.textContent = p1Score;
+        alert('Player 1 wins this round!');
         gameBoard = [
             '', '', '',
             '', '', '',
@@ -179,25 +193,28 @@ function getWinner() {
         p2Score += 1;
         p2Display.classList.add('has-text-success');
         p2Display.innerHTML = p2Score;
+        alert('Player 2 wins this round!');
         gameBoard = [
             '', '', '',
             '', '', '',
             '', '', ''
             ];
-    }  if (square[2].textContent === 'X' & square[4].textContent === 'X' & square[6].textContent === 'X') {
+    }  if (square[2].textContent === 'X' && square[4].textContent === 'X' && square[6].textContent === 'X') {
         p1Score += 1;
         p1Display.classList.add('has-text-success');
         p1Display.innerHTML 
         p1Display.textContent = p1Score;
+        alert('Player 1 wins this round!');
         gameBoard = [
             '', '', '',
             '', '', '',
             '', '', ''
             ];          
-    } if (square[2].textContent === 'O' & square[4].textContent === 'O' & square[6].textContent === 'O') {
+    } if (square[2].textContent === 'O' && square[4].textContent === 'O' &&square[6].textContent === 'O') {
         p2Score += 1;
         p2Display.classList.add('has-text-success');
         p2Display.innerHTML = p2Score;
+        alert('Player 2 wins this round!');
         gameBoard = [
             '', '', '',
             '', '', '',
@@ -214,16 +231,7 @@ function getWinner() {
         p2Display.classList.add('has-text-success')
         p1Display.classList.add('has-text-danger')
         location.reload();
-    }
-}
-
-function playTurn() {
-    let idx = square.findIndex(function(play) {
-        return play === event.target;
-    });
-    gameBoard[idx] = turn;
-    turn = turn === 'X' ? 'O' : 'X';
-    win = getWinner();
+    } 
     game();
 }
 
